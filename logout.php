c647337eb7f831bad93230b98b3ab2238bd88310
wafiq1211 <70130091+wafiq1211@users.noreply.github.com>
2021-10-06 16:08:59 +0800

    logout function
    Prints message of logout successful, removes session and cookies, also has button to go back to login page, or home page or any default page

---
+++ logout.php
@@ -0,0 +1,20 @@
+<body>
+	<?php
+		session_start();
+		session_destroy();
+		if(isset($_COOKIE['username']) && isset($_COOKIE["pw"])) {
+			$name = $_COOKIE["username"];
+			$password = $_COOKIE["pw"];
+			setcookie("username", $name, time() - 1);
+			setcookie("pw", $password, time() - 1);
+		}
+		//echo "<h1>Logout successful</h1>";
+		//echo "You have successfully logged out.";
+		//echo "Click here to <a href='rememberMeTest.php'>login again</a>";
+	?>
+	<h1>Logout successful</h1>
+	You have successfully logged out.
+	<form action="rememberMeTest.php">
+		Click here to <input type="submit" value="Login" />
+	</form>	
+</body>
