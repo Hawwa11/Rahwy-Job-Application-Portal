695a6a3aa94b010a54a6fd001be0255f89822d24
Wafiq Zariful <wafiq.zariful@gmail.com>
2021-10-12 02:57:06 +0800

    removed a useless file the I created

--- imageTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include("db.php");
-
-    $sql = "SELECT * FROM form WHERE id = 8"; 
-    $result = mysqli_query($conn, $sql);
-    $image = mysqli_fetch_array($result);
-    echo '<img src="data:'.$image['coverLetterType'].';base64,'.base64_encode( $image['coverLetter'] ).'"/>';
-?>
