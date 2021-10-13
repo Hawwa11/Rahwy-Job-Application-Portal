c276f83747cc5e40bb5d542f2deb1bbb52bbbbaa
Wafiq Zariful <wafiq.zariful@gmail.com>
2021-10-13 11:13:33 +0800

    modified logout

--- logout.php
+++ logout.php
@@ -3,10 +3,8 @@
 		//remove existing sessions
 		session_start();
 		session_destroy();
+
+		// Direct user to Login page
+		header("Location:Login.php");
 	?>
-	<h1>Logout successful</h1>
-	You have successfully logged out.
-	<form action="Login.php">
-		Click here to <input type="submit" value="Login" />
-	</form>	
 </body>
